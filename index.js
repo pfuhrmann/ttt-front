badedf76491dbd134d6ae768cc8122d566ee3186
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2017-07-15 21:45:29 +0100

    Programatic generation of the board based on API

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
-var $ = require("jquery");
+"use strict";
 
-$('div.cell').on('click', onCellClick);
+let $ = require('jquery');
+let axios = require('axios');
 
-function onCellClick() {
-    var cell = $(this);
-    var position = getCellLocation(cell);
+$(document).ready(function() {
+    function onCellClick() {
+        let cell = $(this);
+        let position = getCellLocation(cell);
 
-    console.log(position[0] + ' x ' + position[1]);
+        console.log(position[0] + ' x ' + position[1]);
 
-    placeX(cell);
-}
-
-function getCellLocation(cell) {
-    var $posA =  cell.parent().index() + 1;
-    var $posB = cell.index() + 1;
-
-    return [$posA, $posB]
-}
-
-function placeX(cell) {
-    if (!cell.html()) {
-        var img = $("<img />",{"src":'images/ttt-x.png'});
-        cell.html(img);
+        placeX(cell);
     }
-}
+
+    function getCellLocation(cell) {
+        let $posA =  cell.parent().index() + 1;
+        let $posB = cell.index() + 1;
+
+        return [$posA, $posB]
+    }
+
+    function placeX(cell) {
+        if (!cell.html()) {
+            let img = $("<img />",{"src":'images/ttt-x.png'});
+            cell.html(img);
+        }
+    }
+
+    function initBoard() {
+        axios.get('http://localhost:8888/api/init')
+            .then(function (response) {
+                drawBoard(response.data.layout);
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function drawBoard(layout) {
+        let board = $('#board');
+
+        for (var i=0; i<layout.length; i++) {
+            let row = layout[i];
+            let rowEl = $('<div class="row"></div>').appendTo(board);
+
+            for (var j=0; j<row.length; j++) {
+                let column = row[j];
+                let columnEl = $('<div class="cell"></div>')
+                    .appendTo(rowEl)
+                    .on('click', onCellClick);
+
+                if (j !== 0 && j !== row.length - 1) {
+                    columnEl.addClass('v');
+                }
+
+                if (i !== 0 && i !== layout.length - 1) {
+                    columnEl.addClass('h');
+                }
+            }
+        }
+    }
+
+    initBoard();
+});
